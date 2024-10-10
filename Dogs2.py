e9207a96674ff6180196708f41e37d7000b010a3
Dogs <ronincitadel@gmail.com>
2024-10-10 13:35:24 +0300

    Cоздана функция изображения в метку

--- Dogs2.py
+++ Dogs2.py
@@ -1,8 +1,26 @@
 from tkinter import *
+from tkinter import messagebox as mb
 import requests
 from PIL import Image,ImageTk
 from io import BytesIO
 
+
+
+
+
+def show_image():
+    image_url = get_dog_image()
+    if image_url:
+        try:
+            response = requests.get(image_url,steam=True)
+            response.rais_for_status()
+            img_data = BytesIo(response.content)
+            img=Image.opem(img_data)
+            img.thumbnail((300,300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            mb.showerror("Ошибка",f"Возникла ошибка {e}")
 
 
 window = Tk()
